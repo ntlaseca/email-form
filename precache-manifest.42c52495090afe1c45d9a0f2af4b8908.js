4f328de3e29b50ecc0c16d3b3f7cadab6d844328
ntlaseca <nate.tlaseca@gmail.com>
2020-04-27 14:27:30 -0500

    adding files

---
+++ precache-manifest.42c52495090afe1c45d9a0f2af4b8908.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94f622fc48babb4e062b5746189997f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a65717e618b01454884e",
+    "url": "/static/css/main.872678e3.chunk.css"
+  },
+  {
+    "revision": "f6b93cfd3e6ffa893175",
+    "url": "/static/js/2.b49fb467.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.b49fb467.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a65717e618b01454884e",
+    "url": "/static/js/main.128e8898.chunk.js"
+  },
+  {
+    "revision": "b351c15c51f85de5fd87",
+    "url": "/static/js/runtime-main.c2d98ea5.js"
+  }
+]);
